4a353ebd64b32724a09d542cc8ea67f1ec6fdceb
Gosheni <euihyunc31@gmail.com>
2022-09-22 14:17:45 -0400

    Added stubs to all helper functions

--- assign2/csf_assign02/c_drawing_funcs.c
+++ assign2/csf_assign02/c_drawing_funcs.c
@@ -9,38 +9,108 @@
 // Helper functions
 ////////////////////////////////////////////////////////////////////////
 
-static int32_t in_bounds(struct Image *img, int32_t x, int32_t y) {
+// Tests whether the given x and y coordinates
+// of the destination image is within the 
+// boundaries of the destination image.
+//
+// Parameters:
+//   img     - pointer to Image (dest image)
+//   x       - x coordinate of location where tile should be copied
+//   y       - y coordinate of location where tile should be copied
+//
+int32_t in_bounds(struct Image *img, int32_t x, int32_t y) {
   if (x < 0 || y < 0 || x >= img->width || y >= img->height) {
     return 0;
   }
   return 1;
 }
 
-static uint32_t compute_index(struct Image *img, int32_t x, int32_t y) {
+// Returns the index of the destination image
+// given its x and y coordinates
+//
+// Parameters:
+//   img     - pointer to Image (dest image)
+//   x       - x coordinate of location where tile should be copied
+//   y       - y coordinate of location where tile should be copied
+//
+uint32_t compute_index(struct Image *img, int32_t x, int32_t y) {
   return y * (img->width) + x;
 }
 
-static uint8_t get_r(uint32_t color) {
+// If the given value is less than min or
+// greater than the given max, the value
+// is modified to fit that min and max range
+//
+// Parameters:
+//   val     - int32_t value
+//   x       - x coordinate of location where tile should be copied
+//   y       - y coordinate of location where tile should be copied
+//
+int32_t clamp(int32_t val, int32_t min, int32_t max) {
+  if (val < min) {
+    val = min;
+  } else if (val > max) {
+    val = max;
+  }
+  return val;
+}
+
+// Returns uint8_t value of the red pixel
+//
+// Parameters:
+//   color - uint32_t color value
+//
+uint8_t get_r(uint32_t color) {
   return color >> 24;
 }
 
-static uint8_t get_g(uint32_t color) {
+// Returns uint8_t value of the green pixel
+//
+// Parameters:
+//   color - uint32_t color value
+//
+uint8_t get_g(uint32_t color) {
   return color >> 16;
 }
 
-static uint8_t get_b(uint32_t color) {
+// Returns uint8_t value of the blue pixel
+//
+// Parameters:
+//   color - uint32_t color value
+//
+uint8_t get_b(uint32_t color) {
   return color >> 8;
 }
 
-static uint8_t get_a(uint32_t color) {
+// Returns uint8_t value of alpha (opacity)
+//
+// Parameters:
+//   color - uint32_t color value
+//
+uint8_t get_a(uint32_t color) {
   return color;
 }
 
-static uint8_t blend_components(uint32_t fg, uint32_t bg, uint32_t alpha) {
+// Blends the foreground color with the given
+// background color with the alpha value at 8 bit scale 
+// 
+// Parameters:
+//   fg    - uint32_t color value of the foreground
+//   bg    - uint32_t color value of the background
+//   alpha - uint32_t alpha value of the opacity
+//
+uint8_t blend_components(uint32_t fg, uint32_t bg, uint32_t alpha) {
   return (alpha * fg + (255 - alpha) * bg) / 255;
 }
 
-static uint32_t blend_colors(uint32_t fg, uint32_t bg) {
+// Blends the foreground color 
+// with the given background color 
+// 
+// Parameters:
+//   fg - uint32_t color value of the foreground
+//   bg - uint32_t color value of the background
+//
+uint32_t blend_colors(uint32_t fg, uint32_t bg) {
   uint32_t color = blend_components(get_r(fg), get_r(bg), get_a(fg));
   color <<= 8;
   color |= blend_components(get_g(fg), get_g(bg), get_a(fg));
@@ -51,15 +121,34 @@ static uint32_t blend_colors(uint32_t fg, uint32_t bg) {
   return color;
 }
 
-static void set_pixel(struct Image *img, uint32_t index, uint32_t color) {
+// Sets the pixel of the destination image
+// at given index with the given color 
+// 
+// Parameters:
+//   img   - pointer to struct Image
+//   index - uint32_t value of index of the image
+//   color - uint32_t color value
+//
+void set_pixel(struct Image *img, uint32_t index, uint32_t color) {
   img->data[index] = blend_colors(color, img->data[index]);
 }
 
-static int64_t square(int64_t x) {
+// Returns int64_t square value of x
+//
+// Parameters:
+//   x - int64_t value
+//
+int64_t square(int64_t x) {
   return x * x;
 }
 
-static int64_t square_dist(int64_t x1, int64_t y1, int64_t x2, int64_t y2) {
+// Returns int64_t distance between 
+// two coordinates (x1,y1) and (x2, y2)
+//
+// Parameters:
+//   x - int64_t value
+//
+int64_t square_dist(int64_t x1, int64_t y1, int64_t x2, int64_t y2) {
   return square(x2-x1) + square(y2-y1);
 }
 
@@ -143,11 +232,14 @@ void draw_tile(struct Image *img,
                int32_t x, int32_t y,
                struct Image *tilemap,
                const struct Rect *tile) {
-  if (x < 0 || y < 0 || x+tile->width >= img->width || y+tile->height >= img->height) {
+  if (tile->x < 0 || tile->y < 0 || 
+  tile->x + tile->width > tilemap->width || tile->y + tile->height > tilemap->height) {
     return;
   }
-  for (int32_t i = 0; i < tile->height; i++) {
-    for (int32_t j = 0; j < tile->width; j++) {
+  int32_t a = clamp(tile->width, 0, img->width-x);
+  int32_t b = clamp(tile->height, 0, img->height-y);
+  for (int32_t i = 0; i < b; i++) {
+    for (int32_t j = 0; j < a; j++) {
       uint32_t color = tilemap->data[compute_index(tilemap, j + tile->x, i + tile->y)];
       img->data[compute_index(img, j+x, i+y)] = color;
     }
@@ -173,11 +265,14 @@ void draw_sprite(struct Image *img,
                  int32_t x, int32_t y,
                  struct Image *spritemap,
                  const struct Rect *sprite) {
-  if (x < 0 || y < 0 || x + sprite->width >= img->width || y + sprite->height >= img->height) {
+  if (sprite->x < 0 || sprite->y < 0 ||
+  sprite->x + sprite->width > spritemap->width || sprite->y + sprite->height > spritemap->height) {
     return;
   }
-  for (int32_t i = 0; i < sprite->height; i++) {
-    for (int32_t j = 0; j < sprite->width; j++) {
+  int32_t a = clamp(sprite->width, 0, img->width-x);
+  int32_t b = clamp(sprite->height, 0, img->height-y);
+  for (int32_t i = 0; i < b; i++) {
+    for (int32_t j = 0; j < a; j++) {
       uint32_t color = spritemap->data[compute_index(spritemap, j + sprite->x, i + sprite->y)];
       draw_pixel(img, j+x, i+y, color);
     }
